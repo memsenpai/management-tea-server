80a318f2fbd5a47cc18191f71feb9c8ed8428701
memsenpai <hung.dao.van@bunbusoft.com>
2021-01-30 09:31:43 +0700

    ignore routes unnecessary

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :follows
-        resources :sleeps
+        resources :follows, only: [:index, :create, :destroy]
+        resources :sleeps, only: [:index, :create, :update]
       end
     end
   end
